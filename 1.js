ddb9d20426107abf14887448c02a2cd6530e859a
bbbb112 <143480938+bbbb112@users.noreply.github.com>
2024-02-16 06:57:06 +0000

    Update 1.js

--- 1.js
+++ 1.js
@@ -7,8 +7,8 @@
 [rewrite_local]
 #修改
 
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/bbbb112/what/main/1.js
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/bbbb112/what/main/1.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/bbbb112/what/main/1.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/bbbb112/what/main/1.js
 
 
 [mitm] 
@@ -17,13 +17,11 @@ hostname = api.revenuecat.com
 
 ************************************/
 
+
+
+
 const Q = {};
 const Q1 = JSON.parse(typeof $response != "undefined" && $response.body || null);
-
-const name = "Premium ";
-const appid = "Speak_399k_1y";
-
-
 if (typeof $response == "undefined") {
   delete $request.headers["x-revenuecat-etag"];
   delete $request.headers["X-RevenueCat-ETag"];
@@ -31,16 +29,39 @@ if (typeof $response == "undefined") {
 } else if (Q1 && Q1.subscriber) {
   Q1.subscriber.subscriptions = Q1.subscriber.subscriptions || {};
   Q1.subscriber.entitlements = Q1.subscriber.entitlements || {};
-  const data = {
-	      "product_identifier": (appid),
-	      "purchase_date": "2023-02-09T09:09:09Z",
-	      "expires_date": "2099-09-09T09:09:09Z"
+  var headers = {};
+  for (var key in $request.headers) {
+  const reg = /^[a-z]+$/;
+  if (key === "User-Agent" && !reg.test(key)) {
+    var lowerkey = key.toLowerCase();
+    $request.headers[lowerkey] = $request.headers[key];
+    delete $request.headers[key];
+    }
+  }
+  var UA = $request.headers['user-agent'];
+  const app = '1';
+  const UAMappings = {
+    'Speak':{ name: 'Premium', id: 'Speak_399k_1y'},
+    
     };
-  Q1.subscriber.entitlements[(name)] = (data);
-  Q1.subscriber.subscriptions[(appid)] = {  ...data,"warning": "仅供学习，禁止转载或售卖",	"original_purchase_date": "2022-09-09T09:09:09Z",	"store": "app_store",	"ownership_type": "PURCHASED"};
+
+  const data = {
+    "expires_date": "2099-12-31T12:00:00Z",
+    "original_purchase_date": "2023-09-01T11:00:00Z",
+    "purchase_date": "2023-09-01T11:00:00Z",
+    "ownership_type": "PURCHASED",
+    "store": "app_store"
+  };
+  for (const i in UAMappings) {
+    if (new RegExp(`^${i}`, 'i').test(UA)) {
+      const { name, id } = UAMappings[i];
+      Q1.subscriber.subscriptions = {};
+      Q1.subscriber.subscriptions[id] = data;
+      Q1.subscriber.entitlements[name] = JSON.parse(JSON.stringify(data));
+      Q1.subscriber.entitlements[name].product_identifier = id;
+      break;
+    }
+  }
   Q.body = JSON.stringify(Q1);
 }
 $done(Q);
-
-
-
