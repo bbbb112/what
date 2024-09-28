3b4a342e6bddb593d3ec5dd21e9f96df0bfb1a53
bbbb112 <143480938+bbbb112@users.noreply.github.com>
2024-09-28 19:39:24 +0800

    Update zetle.js

--- zetle.js
+++ zetle.js
@@ -9,7 +9,7 @@
 [rewrite_local]
 ^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/bbbb112/what/main/zetle.js$
 #清理
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/bbbb112/what/main/zetle.js$
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/bbbb112/what/main/zetle.js
 
 [mitm] 
 hostname = api.revenuecat.com
