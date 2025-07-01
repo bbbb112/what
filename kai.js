151d1390afc9a624336fed4762102cde82f3b8b2
bbbb112 <143480938+bbbb112@users.noreply.github.com>
2025-07-01 16:42:04 +0800

    Create kai.js

---
+++ kai.js
@@ -0,0 +1,13 @@
+/*
+
+凯格尔go 
+三个软件合集
+
+
+[rewrite_local]
+https:\/\/(k\.lite01\.com|go\.uuwing\.com|p\.lite01\.com)\/api\/user\/info url script-response-body https://raw.githubusercontent.com/
+
+[mitm]
+hostname = k.lite01.com, go.uuwing.com, p.lite01.com
+
+*/
