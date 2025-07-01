78b504e78e663778c55797ce550b5e20890c82b6
bbbb112 <143480938+bbbb112@users.noreply.github.com>
2025-07-01 16:52:36 +0800

    Update kai.js

--- kai.js
+++ kai.js
@@ -1,13 +1,1 @@
-/*
 
-凯格尔go 
-三个软件合集
-
-
-[rewrite_local]
-https:\/\/(k\.lite01\.com|go\.uuwing\.com|p\.lite01\.com)\/api\/user\/info url script-response-body https://raw.githubusercontent.com/bbbb112/what/raw/refs/heads/main/kai.js
-
-[mitm]
-hostname = k.lite01.com, go.uuwing.com, p.lite01.com
-
-*/
